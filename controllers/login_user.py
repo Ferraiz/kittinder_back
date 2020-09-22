f3712ebd82a5a8299286a288ee00807246bbc547
Silvia <sarnaizgarcia@gmail.com>
2020-09-22 17:37:43 +0200

    Feature get user created

--- controllers/login_user.py
+++ controllers/login_user.py
@@ -11,7 +11,6 @@ from models.user_model import UserModel
 def login_user(user_data):
     user_email = user_data['email']
     expires = timedelta(seconds=int(os.environ.get('EXPIRE_TIME', '60')))
-    print(expires)
     user = UserModel.find_by_email(user_email)
     if user and safe_str_cmp(user.password, user_data['password']):
         access_token = create_access_token(
